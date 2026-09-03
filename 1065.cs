30960af87938c612c6916e38d1c374caba48c4d6
joa01972 <joaovictor.vasconcelos1@aluno.educa.go.gov.br>
2026-09-03 17:33:36 -0300

    Create 1065.cs

---
+++ 1065.cs
@@ -0,0 +1,21 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int pares = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int valor = int.Parse(Console.ReadLine());
+
+            if (valor % 2 == 0) {
+                pares++;
+            }
+        }
+
+        Console.WriteLine($"{pares} valores pares");
+
+    }
+
+} 

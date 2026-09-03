5a087a8a4afe40b5a348b22cc377f4d32cbd4425
joa01972 <joaovictor.vasconcelos1@aluno.educa.go.gov.br>
2026-09-03 18:01:28 -0300

    Create 1005.cs

---
+++ 1005.cs
@@ -0,0 +1,17 @@
+using System; 
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        double A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double media = ((A * 3.5) + (B * 7.5)) / 11.0;
+
+        Console.WriteLine($"MEDIA = {media.ToString("F5", CultureInfo.InvariantCulture)}");
+
+    }
+
+}

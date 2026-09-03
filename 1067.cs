00c77d12b209cb979d9941c7a9371ee2d8460f66
joa01972 <joaovictor.vasconcelos1@aluno.educa.go.gov.br>
2026-09-03 17:39:17 -0300

    1067

---
+++ 1067.cs
@@ -0,0 +1,17 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int x = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= x; i++) {
+            if (i % 2 != 0) {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+
+}
